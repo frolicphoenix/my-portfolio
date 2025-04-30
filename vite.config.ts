e5480337533d255b39d973964621f8dd587cbc4d
foenyxr <pranjalmlokhande@gmail.com>
2025-04-30 06:54:45 -0400

    reverted changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
-import viteImagemin from 'vite-plugin-imagemin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(), 
-    react(),
-    viteImagemin({
-      webp: {
-        quality: 75
-      },
-    })  
-  ],
+  plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
